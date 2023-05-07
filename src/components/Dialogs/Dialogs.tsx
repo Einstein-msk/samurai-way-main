3a2eb4637e7d30ed634bed2d12886a993c75de94
Einstein-msk <nikitadidukh10@gmail.com>
2023-05-07 13:08:22 +0300

    Step 21. Added to Dialogs.tsx content, added dispay: grid to Dialogs.module.css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,34 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        // делаеем страницу Messages. Она будет состоять из двух div. Первая будет - s.dialogs, а вторая - s.messages.
+        // Отобразим их в виде 2х колонок с помощью display: grid в Dialogs.module.css.
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog+" "+ s.active}>
+                    {/*два склееных класса, диалог активен (выбран), будет подсвечен золотым цветом. В Dialogs.module.css
+                    этот класс будет называться .dialogsItems .active*/}
+                    Bogdan
+                </div>
+                <div className={s.dialog}>
+                    Zhenya
+                </div>
+                <div className={s.dialog}>
+                    Egor
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Alexey
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How are you?</div>
+                <div className={s.dialog}>What are you doing today?</div>
+            </div>
+
         </div>
     );
 };
